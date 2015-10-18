929dccee3ff49f35fd30056cf842dd85e4145810
Theo Denovan <theo@t6d.com>
2015-10-18 11:46:21 +1100

    changes to use environment variables instead of Meteor.Settings

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: "particle4dev:cordova-fb",
-    version: "0.11.0-2",
+    version: "0.11.1-2",
     summary: "Login service for Facebook accounts (works with cordova)",
     git: "https://github.com/particle4dev/meteor-accounts-facebook-cordova.git"
 });
--- src/server/serverKey.js
+++ src/server/serverKey.js
@@ -1,47 +1,42 @@
-var profileFields = [];
+var profileFields = [
+          "id",
+          "name",
+          "gender",
+          "location",
+          "email",
+          "first_name",
+          "last_name",
+          "link",
+          "username",
+          "locale",
+          "age_range"
+      ];
 
 CFB.Configure = function (config) {
-    if(Meteor.settings.environment === "production")
-        if(!config || !config.pro.APP_ID || !config.pro.secret) {
-            throw new Error("Meteor settings for accounts-facebook-cordova not configured correctly.");
-        }
-    if(Meteor.settings.environment === "development")
-        if(!config || !config.dev.APP_ID || !config.dev.secret) {
-            throw new Error("Meteor settings for accounts-facebook-cordova not configured correctly.");
-        }
-    if(config.profileFields) {
-        _.each(config.profileFields, function (p) {
-            if(_.indexOf(profileFields, p) == -1)
-                profileFields.push(p); 
-        });
-    }
-    ServiceConfiguration.configurations.remove({
-        service: "facebook"
-    });
-    if(Meteor.settings.environment === "production")
-        ServiceConfiguration.configurations.insert({
-            service: "facebook",
-            appId: config.pro.APP_ID,
-            secret: config.pro.secret
-        });
-    if(Meteor.settings.environment === "development")
-        ServiceConfiguration.configurations.insert({
-            service: "facebook",
-            appId: config.dev.APP_ID,
-            secret: config.dev.secret
-        });  
-    // https://github.com/meteor/meteor/blob/devel/packages/accounts-facebook/facebook.js#L15
-    Accounts.addAutopublishFields({
-        // publish all fields including access token, which can legitimately
-        // be used from the client (if transmitted over ssl or on
-        // localhost). https://developers.facebook.com/docs/concepts/login/access-tokens-and-types/,
-        // "Sharing of Access Tokens"
-        forLoggedInUser: ['services.facebook'],
-        forOtherUsers: [
-            // https://www.facebook.com/help/167709519956542
-            'services.facebook.id', 'services.facebook.username', 'services.facebook.gender'
-        ]
-    });
+  appId = process.env.FACEBOOK_APP_ID
+  appSecret = process.env.FACEBOOK_APP_SECRET
+  if(!appId || !appSecret)
+    throw new Error("Meteor settings for accounts-facebook-cordova not configured correctly. Missing FACEBOOK_APP_ID or FACEBOOK_APP_SECRET environment variable");
+  ServiceConfiguration.configurations.remove({
+      service: "facebook"
+  });
+  ServiceConfiguration.configurations.insert({
+      service: "facebook",
+      appId: appId,
+      secret: appSecret
+  });
+  // https://github.com/meteor/meteor/blob/devel/packages/accounts-facebook/facebook.js#L15
+  Accounts.addAutopublishFields({
+      // publish all fields including access token, which can legitimately
+      // be used from the client (if transmitted over ssl or on
+      // localhost). https://developers.facebook.com/docs/concepts/login/access-tokens-and-types/,
+      // "Sharing of Access Tokens"
+      forLoggedInUser: ['services.facebook'],
+      forOtherUsers: [
+          // https://www.facebook.com/help/167709519956542
+          'services.facebook.id', 'services.facebook.username', 'services.facebook.gender'
+      ]
+  });
 };
 
 CFB.getProfileFields = function () {
